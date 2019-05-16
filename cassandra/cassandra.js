37a5a0cb18c4af1dd5fe99f9cabaf838a7476513
Matúš Tomlein <matus.tomlein@gmail.com>
2019-05-16 16:55:05 +0200

    Add local data center configuration

--- cassandra/cassandra.js
+++ cassandra/cassandra.js
@@ -1,111 +1,146 @@
+var connections = {};
+
+function createConnection(cassandra, connectionOptions, callback) {
+  var key = JSON.stringify(connectionOptions);
+  if (connections[key]) {
+    connections[key](callback);
+  } else {
+    var waiting = [callback];
+    connections[key] = (connectCallback) => {
+      waiting.push(connectCallback);
+    };
+
+    function reconnect() {
+      var connection = new cassandra.Client(connectionOptions);
+      connection.connect((err) => {
+        if (err) {
+          console.log(connectionOptions);
+          console.error(err);
+          waiting.forEach(w => w(err));
+          setTimeout(reconnect, 30000);
+        } else {
+          connections[key] = c => c(null, connection);
+          waiting.forEach(w => w(null, connection));
+          waiting = [];
+        }
+      });
+    }
+
+    reconnect();
+  }
+}
+
 module.exports = function (RED) {
-    "use strict";
-    var cassandra = require('cassandra-driver');
+  "use strict";
+  var cassandra = require('cassandra-driver');
 
-    function CassandraNode(n) {
-        RED.nodes.createNode(this, n);
-        this.hosts = n.hosts;
-        this.port = n.port;
+  function CassandraNode(n) {
+    RED.nodes.createNode(this, n);
+    this.hosts = n.hosts;
+    this.port = n.port;
 
-        this.connected = false;
-        this.connecting = false;
+    this.connected = false;
+    this.connecting = false;
 
-        this.keyspace = n.keyspace;
-        var node = this;
+    this.keyspace = n.keyspace;
+    this.localDataCenter = n.localDataCenter;
+    var node = this;
 
-        function doConnect() {
-            node.connecting = true;
+    function doConnect() {
+      node.connecting = true;
 
-            var authProvider = null;
-            if (node.credentials.user) {
-                authProvider = new cassandra.auth.PlainTextAuthProvider(
-                    node.credentials.user,
-                    node.credentials.password
-                );
-            }
+      var authProvider = null;
+      if (node.credentials.user) {
+        authProvider = new cassandra.auth.PlainTextAuthProvider(
+          node.credentials.user,
+          node.credentials.password
+        );
+      }
 
-            // TODO: Support other port, default is 9042
-            node.connection = new cassandra.Client({
-                contactPoints: node.hosts.replace(/ /g, "").split(","),
-                keyspace: node.keyspace,
-                authProvider: authProvider,
-                protocolOptions: {port: node.port}
-            });
+      // TODO: Support other port, default is 9042
+      var connectionOptions = {
+        contactPoints: node.hosts.replace(/ /g, "").split(","),
+        localDataCenter: node.localDataCenter,
+        keyspace: node.keyspace,
+        authProvider: authProvider,
+      };
+      if (node.port) {
+        connectionOptions.protocolOptions = {port: node.port};
+      }
 
-            node.connection.connect(function (err) {
-                node.connecting = false;
-                if (err) {
-                    node.error(err);
-                    node.tick = setTimeout(doConnect, 30000);
-                } else {
-                    node.connected = true;
-                }
-            });
-
+      createConnection(cassandra, connectionOptions, (err, connection) => {
+        if (err) {
+          node.error(err);
+        } else {
+          node.connecting = false;
+          node.connection = connection;
+          node.connected = true;
         }
+      });
+    }
 
-        this.connect = function () {
-            if (!this.connected && !this.connecting) {
-                doConnect();
-            }
-        }
+    this.connect = function () {
+      if (!this.connected && !this.connecting) {
+        doConnect();
+      }
+    }
 
-        this.on('close', function (done) {
-            if (this.tick) { clearTimeout(this.tick); }
-            if (this.connection) {
-                node.connection.shutdown(function (err) {
-                    if (err) { node.error(err); }
-                    done();
-                });
-            } else {
-                done();
-            }
+    this.on('close', function (done) {
+      if (this.tick) { clearTimeout(this.tick); }
+      if (this.connection) {
+        node.connection.shutdown(function (err) {
+          if (err) { node.error(err); }
+          done();
         });
-    }
-    RED.nodes.registerType("CassandraDatabase",CassandraNode, {
-        credentials: {
-            user: {type: "text"},
-            password: {type: "password"}
-        }
+      } else {
+        done();
+      }
     });
-
-
-    function CassandraNodeIn(n) {
-        RED.nodes.createNode(this,n);
-        this.mydb = n.mydb;
-        this.mydbConfig = RED.nodes.getNode(this.mydb);
-
-        if (this.mydbConfig) {
-            this.mydbConfig.connect();
-            var node = this;
-            this.on("input", function(msg) {
-                var batchMode = Array.isArray(msg.topic);
-                if (!batchMode && typeof msg.topic !== 'string') {
-                    node.error("msg.topic : the query is not defined as a string or as an array of queries");
-                    return;
-                }
-                var resCallback = function(err, result) {
-                    if (err) {
-                        node.error(err,msg);
-                    } else {
-                        msg.payload = result.rows;
-                        node.send(msg);
-                    }
-                };
-
-                if (batchMode) {
-                    node.log("Batching " + msg.topic.length + " CQL queries");
-                    node.mydbConfig.connection.batch(msg.topic, {prepare: true}, resCallback);
-                } else {
-                    node.log("Executing CQL query: ", msg.topic);
-                    var params = msg.payload || [];
-                    node.mydbConfig.connection.execute(msg.topic, params, {prepare: true}, resCallback);
-                }
-            });
-        }
-        else {
-            this.error("Cassandra database not configured");
-        }
+  }
+  RED.nodes.registerType("CassandraDatabase",CassandraNode, {
+    credentials: {
+      user: {type: "text"},
+      password: {type: "password"}
     }
-    RED.nodes.registerType("cassandra", CassandraNodeIn);
+  });
+
+
+  function CassandraNodeIn(n) {
+    RED.nodes.createNode(this,n);
+    this.mydb = n.mydb;
+    this.mydbConfig = RED.nodes.getNode(this.mydb);
+
+    if (this.mydbConfig) {
+      this.mydbConfig.connect();
+      var node = this;
+      this.on("input", function(msg) {
+        var batchMode = Array.isArray(msg.topic);
+        if (!batchMode && typeof msg.topic !== 'string') {
+          node.error("msg.topic : the query is not defined as a string or as an array of queries");
+          return;
+        }
+        var resCallback = function(err, result) {
+          if (err) {
+            node.error(err,msg);
+          } else {
+            msg.payload = result.rows;
+            node.send(msg);
+          }
+        };
+
+        if (batchMode) {
+          node.log("Batching " + msg.topic.length + " CQL queries");
+          node.mydbConfig.connection.batch(msg.topic, {prepare: true}, resCallback);
+        } else {
+          node.log("Executing CQL query: ", msg.topic);
+          var params = msg.payload || [];
+          node.mydbConfig.connection.execute(msg.topic, params, {prepare: true}, resCallback);
+        }
+      });
+    }
+    else {
+      this.error("Cassandra database not configured");
+    }
+  }
+  RED.nodes.registerType("cassandra", CassandraNodeIn);
 }
